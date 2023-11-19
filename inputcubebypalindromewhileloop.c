9ac43d8852c5652b78400c9d379d3f736bf9f0a4
Zainab Mkhtar <146872966+Zainab-Wani@users.noreply.github.com>
2023-11-19 20:00:15 +0530

    Add files via upload

---
+++ inputcubebypalindromewhileloop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int number, sum=0,digit,cube;
+    printf("enter a number");
+    scanf("%d",&number);
+    while(number!=0)
+    {
+        digit=number%10;
+        cube=digit*digit*digit;
+        sum+=cube;
+        number/=10;
+    }
+    printf("%d\n",(digit*digit*digit));
+    printf("sum of digits=%d\n",sum);
+    return 0;
+}
